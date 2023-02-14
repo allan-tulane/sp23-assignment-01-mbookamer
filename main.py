633115e5913534b5b993f96596158a351c58f7bd
mbookamer <79340162+mbookamer@users.noreply.github.com>
2023-02-13 18:22:27 -0600

    Update main.py
    added all functions and commented it out, as well as added more test cases

--- main.py
+++ main.py
@@ -41,12 +41,44 @@ class Result:
               (self.longest_size, self.left_size, self.right_size, self.is_entire_range))
     
     
-def longest_run_recursive(mylist, key):
+def longest_run_recursive_1(mylist, key):
     ### TODO
+    if len(mylist) == 0: #empty list
+        return Result(0, 0, 0, False)
+
+    if len(mylist) == 1: #only 1 element in list
+        if mylist[0] == key:
+            return Result(1,1,1,True)
+        else:
+            return Result(0,0,0,False)
+    else:
+        left = longest_run_recursive_1(mylist[:len(mylist)//2], key) #split in half
+        right = longest_run_recursive_1(mylist[len(mylist)//2:], key)
+        
+        if left.is_entire_range and right.is_entire_range: #the whole list contains the key
+            return Result(left.left_size + right.right_size, left.left_size + right.right_size, left.left_size + right.right_size, True)
+       
+        elif left.is_entire_range: #whole left side is the key
+            return Result(left.left_size + right.left_size, right.right_size, max(left.longest_size, right.longest_size, left.right_size + right.left_size), False)
+        
+        elif right.is_entire_range: #whole right side is the key
+            return Result(left.left_size, right.right_size + left.right_size , max(left.longest_size, right.longest_size, left.right_size + right.left_size), False)
+        
+        else: #mixture on both sides
+            return Result(left.left_size, right.right_size, max(left.longest_size, right.longest_size, left.right_size + right.left_size), False)
     pass
+
+def longest_run_recursive(mylist, key):
+    Result = longest_run_recursive_1(mylist, key) #we want to create a 'helper' function to keep on calling our recursive step
+    return Result.longest_size #we just want to how mant times it occurs 
 
 ## Feel free to add your own tests here.
 def test_longest_run():
     assert longest_run([2,12,12,8,12,12,12,0,12,1], 12) == 3
+    assert longest_run([4,4,4,4,4,0,7,9,9,0], 4) == 5
+    
+def test_longest_run_recursive():
+    assert longest_run_recursive([2,12,12,8,12,12,12,0,12,1], 12) == 3
+    assert longest_run_recursive([4,4,4,4,4,0,7,9,9,0], 4) == 5
 
 
